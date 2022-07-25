2fef9af9e31a8792ff4f498c6bf551d3e8913bae
LyubovP <Lyubovpidoshva@gmail.com>
2022-07-25 17:46:04 +0300

    Add movie scaffold

---
+++ db/migrate/20220725134859_create_movies.rb
@@ -0,0 +1,14 @@
+class CreateMovies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.text :description
+      t.integer :category, default: 0
+      t.string :rating
+      t.integer  :user_id
+
+
+      t.timestamps
+    end
+  end
+end
